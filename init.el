1b6e106a93b8dc7cd28c812a2f96c732a04126a7
Rasadell09 <rasadell09@gmail.com>
2015-06-21 15:37:49 -0700

    Modified error control

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
 ;; no bell noise
 (setq visible-bell t)
 ;; ecb error control
-(setq stack-trace-on-error t)
+;;(setq stack-trace-on-error t)
 ;; -------------------------- end ---------------------------------
 
 ;; -------------------------- global key bind ---------------------
@@ -71,17 +71,16 @@
 (ecb-activate)
 ;; ------------------------- end ----------------------------------
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ecb-source-path
-   (quote
-    (("~/.emacs.d/" ".emacs.d")
-     ("/home/liyunf/gpu-golden/gpgpu-sim_distribution/" "gpgpu-sim")))))
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-layout-window-sizes nil)
+ '(ecb-source-path (quote (("~/.emacs.d/" ".emacs.d") ("/home/liyunf/gpu-golden/gpgpu-sim_distribution/" "gpgpu-sim"))))
+ '(inhibit-startup-screen t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  )
